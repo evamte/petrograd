73ac69d19022158d4206c89834883e4419a37b51
evamte <evax150a@stud.kea.dk>
2020-08-28 12:33:59 +0200

    initial structure

---
+++ js/script.js
@@ -0,0 +1,29 @@
+fetch("https://kea-alt-del.dk/t5/api/")
+    .then(function(response){
+    console.log(response);
+    return response.json();
+})
+    .then(function(data){
+    console.log(data);
+    dataReceived(data);
+})
+
+function dataReceived(products) {
+    //loop through products
+    products.forEach(showProduct);
+}
+
+//executed once for each product
+function showProduct(myProduct) {
+    console.log(myProduct);
+    //finding the template
+    const temp = document.querySelector("#productTemplate").content;
+    //clone the template
+    const myCopy = temp.cloneNode(true);
+    //fill in the template
+    myCopy.querySelector(".data_name").textContent = myProduct.name;
+    //append
+    const parentElement = document.querySelector("section#starter");
+    parentElement.appendChild(myCopy);
+}
+
