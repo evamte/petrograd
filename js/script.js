d4d0447af008aa452666ca5ef3ae193810905372
evamte <evax150a@stud.kea.dk>
2020-09-01 14:12:23 +0200

    created imgs for vegetarian, laktose and alcohol

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 function init() {
-    fetch("https://kea-alt-del.dk/t5/api/categories").then(r=> r.json()).then(
-    function (data) {
-        categoriesReceived(data)
-    })
+    fetch("https://kea-alt-del.dk/t5/api/categories").then(r => r.json()).then(
+        function (data) {
+            categoriesReceived(data)
+        })
 }
 
 init();
 
-function categoriesReceived (cats) {
+function categoriesReceived(cats) {
     //createNavigation(cats);
     createSections(cats);
+    fetchProducts();
 }
 
 function createSections(categories) {
@@ -22,19 +23,21 @@ function createSections(categories) {
         document.querySelector(".productlist").appendChild(section);
     })
 }
-//fetch Products
-fetch("https://kea-alt-del.dk/t5/api/productlist")
-    .then(function (response){
-    console.log(response);
-    return response.json();
-})
-    .then(function (data){
-    dataReceived(data);
-})
 
-function dataReceived(products) {
-    //loop through products
-    products.forEach(showProduct);
+function fetchProducts() {
+    fetch("https://kea-alt-del.dk/t5/api/productlist")
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        })
+        .then(function (data) {
+            dataReceived(data);
+        })
+
+    function dataReceived(products) {
+        //loop through products
+        products.forEach(showProduct);
+    }
 }
 
 //executed once for each product
@@ -44,22 +47,57 @@ function showProduct(myProduct) {
     const temp = document.querySelector("#productTemplate").content;
     //clone the template
     const myCopy = temp.cloneNode(true);
+
+    //img
+    const img = myCopy.querySelector(".product_img");
+    img.setAttribute("src", `https://kea-alt-del.dk/t5/site/imgs/small/${myProduct.image}-sm.jpg`);
     //fill in the template
     myCopy.querySelector(".name").textContent = myProduct.name;
-    const parentElem = document.querySelector("section#" + myProduct.category);
-    //append
-    parentElem.appendChild(myCopy);
+    myCopy.querySelector(".prize").textContent = myProduct.price;
+    myCopy.querySelector(".short").textContent = myProduct.shortdescription;
+    myCopy.querySelector(".long").textContent = myProduct.longdescription;
+    myCopy.querySelector(".alcohol").textContent = myProduct.alcohol;
+
+
+    //discount
+    if (!myProduct.discount) {
+        myCopy.querySelector(".discount").classList.add("hidden");
+    }
+
+    //vegetarian
+        if (myProduct.vegetarian) {
+            myCopy.querySelector(".vegetarian").classList.remove("hidden");
+        }
+
+    //laktose
+
+    if (myProduct.laktose) {
+            myCopy.querySelector(".laktose").classList.remove("hidden");
+        }
+    //soldout
+        if (myProduct.soldout) {
+            const p = document.createElement("p");
+            p.textContent = "Sold Out";
+            p.classList.add("soldout");
+            myCopy.querySelector("article").appendChild(p);
+        }
+
+     const article = myCopy.querySelector("article");
+
+    if (myProduct.vegetarian) {
+        article.classList.add("vegetarian");
+    }
+
+    if (myProduct.laktose) {
+        article.classList.add("laktose");
+    }
+//alcohol
+    if (myProduct.alcohol) {
+        article.classList.add("alcoholic");
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
+    //append
+    const parentElem = document.querySelector("section#" + myProduct.category);
+    parentElem.appendChild(myCopy);
+}
